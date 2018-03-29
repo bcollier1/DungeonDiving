74743175cb471cc37ef7c4a34b5fafe4907249f6
Brady Collier <brady_c_223@hotmail.com>
2018-03-29 10:44:49 -0400

    Finished all the rooms in the first room. Next push will include the first room fully functionable. What will need to be done will be changing the void within void to a variable.

--- DungeonDiving/Starting Room.cpp
+++ DungeonDiving/Starting Room.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 char dir;
 char ans;
+int gear = 0;
+int key = 0;
 
 void r3() {
 	bool r3ans = true;
@@ -54,11 +56,85 @@ void r7() {
 }
 
 void r10() {
+	bool r10ans = true;
+	bool r10ans1 = true;
+	cout << "You have entered a room that contains a rug that has a bulge in the\n";
+	cout << "center of it. Did you want to search to see what's there. (Y or N)\n\n";
+	cin >> ans;
+	while (r10ans) {
+		if (ans == 'Y' || ans == 'y') {
+			cout << "There was a gear under the rug. Do you want to pick it up? (Y or N)\n\n";
+			cin >> ans;
+			while (r10ans1) {
+				if (ans == 'Y' || ans == 'y') {
+					cout << "'You have picked up a gear.'";
+					r10ans1 = false;
+					gear = 1;
+				}
+				else if (ans == 'N' || ans == 'n') {
+					r10ans1 = false;
+				}
+				else {
+					cout << "Wrong answer! Enter Y OR N\n\n";
+					cin >> ans;
+				}
+			}
+		}
+		else if (ans == 'N' || ans == 'n') {
+			cout << "You notice there is a grandfather clock. Do you want to search it?\n\n";
+			cout << "(Y or N)";
+			cin >> ans;
+			if (ans == 'Y' || ans == 'y' && gear == 0) {
+				cout << "You notice that there is a missing gear. Find it and then come back\n\n";
 
+			}
+			if (ans == 'Y' || ans == 'y' && gear == 1) {
+				cout << "You have found the key!";
+				key = 1;
+			}
+			else if (ans == 'N' || ans == 'n') {
+				cout << "There is nothing else to search\n\n";
+				r10ans = false;
+				h11();
+			}
+		}
+	}
 }
 
 void r12() {
-
+	bool r12ans = true;
+	bool r12ans1 = true;
+	cout << "You have entered a room that has a chair. Do you wish to search it? (Y or N)\n\n";
+	cin >> ans;
+	while (r12ans) {
+		if (ans == 'Y' || ans == 'y') {
+			cout << "There is nothing in the chair to search.";
+			r12ans = false;
+		}
+		else if (ans == 'N' || ans == 'n') {
+			r12ans = false;
+		}
+		else {
+			cout << "Wrong answer! Enter Y OR N\n\n";
+			cin >> ans;
+		}
+	}
+	cout << "You notice something flickering in the fire place. Would you like to";
+	cout << "investigate what it is? (Y or N)\n\n";
+	cin >> ans;
+	while (r12ans1) {
+		if (ans == 'Y' || ans == 'y') {
+			cout << "'You have collect the victims phone, it is still functionable'";
+			r12ans1 = false;
+		}
+		else if (ans == 'N' || ans == 'n') {
+			r12ans1 = false;
+		}
+		else {
+			cout << "Wrong answer! Enter Y OR N\n\n";
+		}
+	}
+	cout << "There is nothing left to search in the room!";
 }
 
 void h11() {
@@ -70,9 +146,14 @@ void h11() {
 			h11dir = false;
 			r10();
 		}
-		else if (dir == 'S' || dir == 's') {
+		else if (dir == 'S' || dir == 's' && key == 0) {
+			cout << "You need to unlock the door!";
+		}
+		else if (dir == 'S' || dir == 's' && key == 1) {
+			cout << "Congratulation! You can now enter the next room!\n\n";
 			h11dir = false;
-			h11(); // This will have to be the exit of some sort
+			system("CLS");
+			//room2();
 		}
 		else if (dir == 'E' || dir == 'e') {
 			h11dir = false;
@@ -86,11 +167,59 @@ void h11() {
 }
 
 void r9() {
-
+	bool r9ans = true;
+	bool r9ans1 = true;
+	cout << "You have entered a room. You noticed that there is piano with blood\n\n";
+	cout << "on it. Do you wish to collect a blood sample? (Y or N)\n\n";
+	cin >> ans;
+	while (r9ans) {
+		if (ans == 'Y' || ans == 'y') {
+			cout << "'You have collected a sample of blood'";
+			r9ans = false;
+		}
+		else if (ans == 'N' || ans == 'n') {
+			r9ans = false;
+		}
+		else {
+			cout << "Wrong answer! Enter Y OR N\n\n";
+			cin >> ans;
+		}
+	}
+	cout << "There is a table with a ripped shirt. Do you wish to take it? (Y or N)\n\n";
+	cin >> ans;
+	while (r9ans1) {
+		if (ans == 'Y' || ans == 'y') {
+			cout << "'You have collected a ripped shirt'";
+			r9ans1 = false;
+		}
+		else if (ans == 'N' || ans == 'n') {
+			r9ans1 = false;
+		}
+		else {
+			cout << "Wrong answer! Enter Y OR N\n\n";
+		}
+	}
+	cout << "There is nothing left to search in the room.";
 }
 
 void h6() {
-
+	bool h6dir = true;
+	cout << "You have entered another hallway! Which way would you like to go? (W, S)\n\n";
+	cin >> dir;
+	while (h6dir) {
+		if (dir == 'W' || dir == 'w') {
+			h6dir = false;
+			h5();
+		}
+		else if (dir == 'S' || dir == 's') {
+			h6dir = false;
+			r9();
+		}
+		else {
+			cout << "Wrong answer! Enter W OR S\n\n";
+			cin >> dir;
+		}
+	}
 }
 
 void h8() {
